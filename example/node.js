6f6acb4d79c4c28ed9144d61ccab6d108c5400ef
Sascha Droste <sascha.droste@gmail.com>
2013-09-17 17:09:08 +0200

    restructured; ready to publish it on npm registry

---
+++ example/node.js
@@ -0,0 +1,20 @@
+var murmurHash3 = require("../lib/murmurHash3");
+
+console.log("x86:");
+console.log(murmurHash3.x86.hash32("I will not buy this record, it is scratched."));
+console.log(murmurHash3.x86.hash128("I will not buy this tobacconist's, it is scratched."));
+console.log(murmurHash3.x86.hash32("My hovercraft is full of eels.", 0));
+console.log(murmurHash3.x86.hash32("My hovercraft is full of eels.", 25));
+console.log(murmurHash3.x86.hash32("My hovercraft is full of eels.", 128));
+
+
+console.log("\nx64:");
+console.log(murmurHash3.x64.hash128("I will not buy this record, it is scratched."));
+console.log(murmurHash3.x64.hash128("I will not buy this tobacconist's, it is scratched."));
+console.log(murmurHash3.x64.hash128("My hovercraft is full of eels.", 0));
+console.log(murmurHash3.x64.hash128("My hovercraft is full of eels.", 25));
+console.log(murmurHash3.x64.hash128("My hovercraft is full of eels.", 128));
+
+
+
+
